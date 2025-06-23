1a499f1141cc623f18782f877c63a1474e8fb2dd
Your Name <chhaji.kapil@gmail.com>
2025-06-24 00:09:40 +0800

    add email field

--- lib/screens/home_module/add_new_contact.dart
+++ lib/screens/home_module/add_new_contact.dart
@@ -35,6 +35,8 @@ class ScanQrCodeBottomSheet extends StatefulWidget {
 class _ScanQrCodeBottomSheetState extends State<ScanQrCodeBottomSheet> {
   ContactCubit?_contactCubit;
   TextEditingController firstNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
   TextEditingController lastNameController = TextEditingController();
 
 @override
@@ -305,6 +307,45 @@ class _ScanQrCodeBottomSheetState extends State<ScanQrCodeBottomSheet> {
                         ),
                       ),
                     ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: SizedBox(
+                        height: 45,
+                        child: TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            labelText: AppLocalizations.of(context)
+                                .translate('email'),
+                            fillColor: Colors.grey.shade200,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                      ),
+                    ),const SizedBox(width: 16),
+                    Expanded(
+                      child: SizedBox(
+                        height: 45,
+                        child: TextField(
+                          controller: phoneController,
+                          decoration: InputDecoration(
+                            labelText: AppLocalizations.of(context)
+                                .translate('phoneumber'),
+                            fillColor: Colors.grey.shade200,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          keyboardType: TextInputType.number,
+                        ),
+                      ),
+                    ),
                   ],
                 ),
 
@@ -400,9 +441,16 @@ class _ScanQrCodeBottomSheetState extends State<ScanQrCodeBottomSheet> {
                        child: ElevatedButton(
                          // iconAlignment: IconAlignment.start,
                          onPressed: () {
-                           if(firstNameController.text.isNotEmpty && lastNameController.text.isNotEmpty && _selectedImage != null){
+                           if(firstNameController.text.isNotEmpty && lastNameController.text.isNotEmpty
+                               && emailController.text.isNotEmpty && phoneController.text.isNotEmpty
+                               && _selectedImage != null){
+                             if(!emailRegex.hasMatch(emailController.text)){
+                               Utility().showFlushBar(context: context, message:"Enter a valid email address.");
+                             }else{
                            Utility.showLoader(context);
-                           submitData(firstName: firstNameController.text,context: context,lastName: lastNameController.text,cardImage: _selectedImage ?? File(""));}
+                           submitData(
+                               email: emailController.text,phone: phoneController.text,
+                               firstName: firstNameController.text,context: context,lastName: lastNameController.text,cardImage: _selectedImage ?? File(""));}}
                            else{
                              Utility().showFlushBar(context: context, message: "please fill all field",isError: true);
                            }
@@ -441,9 +489,13 @@ class _ScanQrCodeBottomSheetState extends State<ScanQrCodeBottomSheet> {
     );
   }
 
+  final emailRegex = RegExp(r'^[^@\s]+@[^@\s]+\.[^@\s]+$');
+
   Future<void> submitData({
     required String firstName,
     required String lastName,
+    required String email,
+    required String phone,
     required BuildContext context,
     required File cardImage, // Card image file
   }) async {
@@ -459,7 +511,9 @@ class _ScanQrCodeBottomSheetState extends State<ScanQrCodeBottomSheet> {
     }else{
       data = FormData.fromMap({
         'first_name': firstName,
-        'last_name': lastName
+        'last_name': lastName,
+        'email': email,
+        'phone':phone
       });
     }
     _contactCubit?.apiAddPhysicalCard(data);
